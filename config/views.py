97ece30a371704adcc873ad0109a9dac42b9a239
azrielpamungkas <github.rwcoy@simplelogin.co>
2022-06-17 10:19:05 +0700

    Add address in response get SubmitAttendance

--- config/views.py
+++ config/views.py
@@ -1,4 +1,6 @@
+import json
 from time import time
+from types import NoneType
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
@@ -11,15 +13,21 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
 from drf_yasg.utils import swagger_auto_schema
 from geopy import distance
+from geopy.geocoders import Nominatim
 from . import serializers
 import datetime
 import random
 
 
-def c_lecture(user):
+def current_lecture(user):
     try:
-        current_lecture = Timetable.objects.all().filter(date=datetime.date.today()).filter(start_time__lte=datetime.datetime.now(
-        ).time()).filter(end_time__gt=datetime.datetime.now().time()).filter(subject__classroom__student=user).first()
+        current_lecture = Timetable.objects.all(
+
+        ).filter(date=datetime.date.today()
+                 ).filter(start_time__lte=datetime.datetime.now(
+                 ).time()).filter(
+            end_time__gt=datetime.datetime.now().time()
+        ).filter(subject__classroom__student=user).first()
         # current_lecture = Timetable.objects.get(date=datetime.date.today(), start_time__lte=datetime.datetime.now().time(), end_time__gt=datetime.datetime.now().time(),subject__classroom__student=user)
         return current_lecture
     except ObjectDoesNotExist:
@@ -27,70 +35,123 @@ def c_lecture(user):
 
 
 class SubmitAttendance(APIView):
+    """
+        Untuk Submit Absensi
+        path: student/submit/
+    """
     serializer_class = serializers.SubmitAttendanceSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
-        if c_lecture(request.user.id):
-            data = c_lecture(request.user.id)
+        try:
+            lat = self.request.query_params.get('lat')
+            lng = self.request.query_params.get('lng')
+            geolocator = Nominatim(user_agent="coordinateconverter")
+            location = geolocator.reverse(lat+", "+lng).address
+        except:
+            location = "Antah Berantah"
+        if current_lecture(request.user.id):
+            data = current_lecture(request.user.id)
             if StudentAttendance.objects.filter(
                     name="{} {}".format(
                         request.user.first_name, request.user.last_name),
                     timetable=data.id):
-                return Response({'status_code': 14, 'message': 'Anda sudah absen', 'data': {'name': data.subject.name, 'teacher': "{} {}".format(data.subject.teacher.first_name, data.subject.teacher.last_name), 'time': "{} - {} WIB".format(data.start_time.strftime("%H:%M"),
-                                                                                                                                                                                                                                                data.end_time.strftime("%H:%M"))}})
-            return Response({'status_code': 11, 'message': 'ada kelas', 'data': {'name': data.subject.name, 'teacher': "{} {}".format(data.subject.teacher.first_name, data.subject.teacher.last_name),
-                                                                                 'date': data.date, 'token': data.token,
-                             'time': "{} - {} WIB".format(data.start_time.strftime("%H:%M"),
-                                                          data.end_time.strftime("%H:%M"))},
-                             })
+                return Response(
+                    {'status_code': 14,
+                     'message': 'Anda sudah absen',
+                     'data': {
+                         'name': data.subject.name,
+                         'address': location.address,
+                         'teacher': "{} {}".format(
+                             data.subject.teacher.first_name,
+                             data.subject.teacher.last_name),
+                         'time': "{} - {} WIB".format(
+                             data.start_time.strftime("%H:%M"),
+                             data.end_time.strftime("%H:%M")
+                         )}}
+                )
 
-        return Response({'message': 'tidak ada kelas saat ini', 'data': {}})
+            return Response(
+                {'status_code': 11,
+                 'message': 'ada kelas',
+                 'data': {
+                     'name': data.subject.name,
+                     'teacher': "{} {}".format(
+                         data.subject.teacher.first_name,
+                         data.subject.teacher.last_name),
+                     'date': data.date, 'token': data.token,
+                     'time': "{} - {} WIB".format
+                     (data.start_time.strftime("%H:%M"),
+                      data.end_time.strftime("%H:%M"))},
+                 })
+
+        return Response({'status_code': 16,
+                         'address': location,
+                         'message': 'tidak ada kelas saat ini',
+                         'data': {}
+                         })
 
     @swagger_auto_schema(request_body=serializer_class)
     def post(self, request):
-        if c_lecture(request.user.id):
+        if current_lecture(request.user.id):
             submit_data = request.data.copy()
             submit_data['name'] = "{} {}".format(
                 request.user.first_name, request.user.last_name)
             try:
-                submit_data['timetable'] = c_lecture(request.user.id).id
+                submit_data['timetable'] = current_lecture(request.user.id).id
             except:
-                return Response('tidak ada kelas')
+                return Response('Unauthorized')
 
+            radius = 0.06
             center_point = [
-                {'lat': -6.990244896200767, 'lng': 110.42050151945978}]
-            # center_point = [{'lat': -7.9120362, 'lng': 110.3428084}]
+                {'lat': -6.990244896200767,
+                 'lng': 110.42050151945978}
+            ]
+
             test_point = [
-                {'lat': submit_data['lat'], 'lng': submit_data['lng']}]
-            radius = 0.06  # in kilometer
+                {'lat': submit_data['lat'],
+                 'lng': submit_data['lng']}
+            ]
+
             center_point_tuple = tuple(center_point[0].values())
             test_point_tuple = tuple(test_point[0].values())
+
             try:
                 dis = distance.great_circle(
                     center_point_tuple, test_point_tuple).km
             except ValueError:
-                return Response({'message': 'Lokasi anda tidak valid', 'data': {}})
+                return "Lokasi anda tidak valid"
 
-            # if submit_data['token'] == c_lecture(request.user.id).token:
-            if submit_data['token'] == 'TEST':
+            # Check Token
+            if submit_data['token'] == current_lecture(request.user.id).token:
+                # Check Coordinate
                 if dis <= radius:
-                    # if True:
                     del submit_data['lat']
                     del submit_data['lng']
                     serializer = serializers.SubmitAttendanceSerializer(
                         data=submit_data)
                     if serializer.is_valid():
-                        if not StudentAttendance.objects.filter(name=submit_data['name'], timetable=submit_data['timetable']).exists():
+                        if not StudentAttendance.objects.filter(
+                            name=submit_data['name'],
+                            timetable=submit_data['timetable']
+                        ).exists():
                             serializer.save(
-                                name=submit_data['name'], timetable=Timetable.objects.get(id=submit_data['timetable']))
-                            return Response({'status_code': 15, 'message': 'Anda Berhasil Absen', 'data': serializer.data})
-                        return Response({'status_code': 14, 'message': "absen", 'data': {}})
-                    else:
-                        return Response({'data': serializer.errors})
-                return Response({'status_code': 13, 'message': 'Anda diluar titik point'})
+                                name=submit_data['name'],
+                                timetable=Timetable.objects.get(
+                                    id=submit_data['timetable']))
+                            return Response(
+                                {'status_code': 15,
+                                 'message': 'Anda Berhasil Absen',
+                                 'data': serializer.data})
+                    return Response({'status_code': 911,
+                                     'data': serializer.errors})
+                # Error Coordinate
+                return Response(
+                    {'status_code': 13,
+                     'message': 'Anda diluar titik point'})
+                # Error Token
             return Response({"status_code": 12, "message": "Token anda salah"})
-        return Response({'status_code': 17, 'message': 'Tidak ada kelas'})
+        return None
 
 
 class StudentDashboard(APIView):
@@ -107,15 +168,9 @@ class StudentDashboard(APIView):
             greeting = "Selamat Sore"
         else:
             greeting = "Selamat Malam"
-        quote = random.choice([
-            'Jangan bermain-main dengan perasaan orang lain. Jangan, itu menyakitkan. Dan besok lusa, boleh jadi giliran perasaan kita yang dijadikan mainan orang lain.',
-            'Kalau kita menyelamatkan hati orang lain, suatu saat Tuhan pasti akan menyelamatkan kita.',
-            'Hidupmu hanya kamu yang menjalani. Jadi, apa pun keputusan yang kamu ambil harus kamu jalankan dengan sepenuh hati.',
-            'Bukan tempatnya yang sempit, hatinya saja yang kurang lapang. Bukan rezekinya yang sedikit, bersyukurnya saja yang kurang.',
-            'Cinta tak selamanya indah, tetapi tergantung siapa yang menjalaninya',
-            'Tidak perlu kata-kata yang penting bukti nyata'
-            'Masa lalumu milikmu, masa laluku milikku',
-        ])
+        with open('quote.json') as f:
+            file = json.load(f)
+        quote = random.choice(file['data'])
         user = User.objects.get(id=request.user.id)
         name = "{} {}".format(user.first_name, user.last_name)
         data = {'status_code': '000',
@@ -125,7 +180,7 @@ class StudentDashboard(APIView):
                          'name': name,
                          }}
         try:
-            current_lecture = c_lecture(request.user.id)
+            current_lecture = current_lecture(request.user.id)
 
             data['data']['current_lecture'] = {
                 'subject': current_lecture.subject.name,
