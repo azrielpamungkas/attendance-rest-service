1a06823289c122642fd8460f6d51c88c83084557
azrielpamungkas <github.rwcoy@simplelogin.co>
2022-07-07 15:02:19 +0700

    Add TeacherSchedule views

--- teacher/urls.py
+++ teacher/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("v1/teacher/", views.TeacherDashboard.as_view()),
     path("v1/teacher/class/", views.DetailPresence.as_view()),
     path("v1/teacher/info/", views.TeacherInfo.as_view()),
+    path("v1/teacher/schedule/", views.TeacherSchedule.as_view()),
 ]
